be27371e848df137795317e3c7e574cc36e0d283
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-18 20:50:00 -0500

    Add project form functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import './style.css';
 
+const projectList = document.getElementById('project-list');
+
 const form = document.querySelector('form');
 form.addEventListener('submit', e => {
     e.preventDefault();
+    const inputText = form.elements['project-input'].value;
+
+    const li = document.createElement('li');
+    li.classList.add('project');
+    li.textContent = inputText;
+    projectList.appendChild(li);
+    form.reset();
 })
 
 const formContainer = document.querySelector('.form-container');
@@ -15,6 +24,7 @@ addProjectBtn.addEventListener('click', () => {
 const cancelProjectBtn = document.getElementById('cancel-create-project-btn');
 cancelProjectBtn.addEventListener('click', () => {
     formContainer.hidden = true;
+    form.reset();
 })
 
 const createProjectBtn = document.getElementById('create-project-btn');
