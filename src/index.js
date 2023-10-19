f24bcd16283c71ad6b9f6f75d9bb7d0b914eab58
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-18 20:23:25 -0500

    Add form for adding projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
-console.log('test');
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+})
+
+const formContainer = document.querySelector('.form-container');
+
+const addProjectBtn = document.getElementById('add-project-btn');
+addProjectBtn.addEventListener('click', () => {
+    formContainer.hidden = false;
+})
+
+const cancelProjectBtn = document.getElementById('cancel-create-project-btn');
+cancelProjectBtn.addEventListener('click', () => {
+    formContainer.hidden = true;
+})
+
+const createProjectBtn = document.getElementById('create-project-btn');
+createProjectBtn.addEventListener('click', () => {
+    formContainer.hidden = true;
+})
