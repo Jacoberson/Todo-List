bd1cb88c16fdb0ec1d7dc6df987fde73a65151b6
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-18 19:56:17 -0500

    Start basic skeleton and styling

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,21 @@ module.exports = {
     devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development',
+            title: 'Todo List',
+            template: 'src/template.html',
         }),
     ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
