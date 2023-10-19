a93f41099f086db5d2e34ba324b72a9e73c18837
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-18 21:27:37 -0500

    Separate form functionality to separate module

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 import './style.css';
+import Form from './components/Form';
 
 const projectList = document.getElementById('project-list');
 
 const form = document.querySelector('form');
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    const inputText = form.elements['project-input'].value;
-
-    const li = document.createElement('li');
-    li.classList.add('project');
-    li.textContent = inputText;
-    projectList.appendChild(li);
-    form.reset();
+    Form.submitForm(form, e, projectList);
 })
 
 const formContainer = document.querySelector('.form-container');
 
-const addProjectBtn = document.getElementById('add-project-btn');
+const addProjectBtn = document.getElementById('open-project-form');
 addProjectBtn.addEventListener('click', () => {
-    formContainer.hidden = false;
-    addProjectBtn.hidden = true;
+    Form.showForm(formContainer, addProjectBtn);
 })
 
 const cancelProjectBtn = document.getElementById('cancel-create-project-btn');
 cancelProjectBtn.addEventListener('click', () => {
-    formContainer.hidden = true;
-    addProjectBtn.hidden = false;
-    form.reset();
+    Form.hideForm(formContainer, addProjectBtn);
+    Form.resetForm(form);
 })
 
 const createProjectBtn = document.getElementById('create-project-btn');
 createProjectBtn.addEventListener('click', () => {
-    formContainer.hidden = true;
-    addProjectBtn.hidden = false;
+    Form.hideForm(formContainer, addProjectBtn);
 })
