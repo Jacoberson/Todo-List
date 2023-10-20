bdbda6e86fdebc9aeb679c69091e4a1a0beb43f1
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-19 19:25:12 -0500

    Add visibilityChanger module

---
+++ src/helpers/visibilityChanger.js
@@ -0,0 +1,9 @@
+const showElement = element => {
+    return element.hidden = false;
+}
+
+const hideElement = element => {
+    return element.hidden = true;
+}
+
+export { showElement, hideElement }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { showElement, hideElement } from './helpers/visibilityChanger';
 
 const projectList = document.getElementById('project-list');
 
@@ -16,21 +17,21 @@ form.addEventListener('submit', e => {
 
 const formContainer = document.querySelector('.form-container');
 
-const addProjectBtn = document.getElementById('open-project-form');
-addProjectBtn.addEventListener('click', () => {
-    formContainer.hidden = false;
-    addProjectBtn.hidden = true;
+const openProjectForm = document.getElementById('open-project-form');
+openProjectForm.addEventListener('click', () => {
+    showElement(formContainer);
+    hideElement(openProjectForm);
 })
 
 const cancelProjectBtn = document.getElementById('cancel-project-btn');
 cancelProjectBtn.addEventListener('click', () => {
-    formContainer.hidden = true;
-    addProjectBtn.hidden = false;
+    hideElement(formContainer);
+    showElement(openProjectForm);
     form.reset();
 })
 
 const createProjectBtn = document.getElementById('create-project-btn');
 createProjectBtn.addEventListener('click', () => {
-    formContainer.hidden = true;
-    addProjectBtn.hidden = false;
+    hideElement(formContainer);
+    showElement(openProjectForm);
 })
