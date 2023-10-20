dd2c86ba5cf0ea5beb3ace014538d7cd5cf78844
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-19 19:08:21 -0500

    Revert "Separate form functionality to separate module"
    This reverts commit a93f41099f086db5d2e34ba324b72a9e73c18837.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import './style.css';
-import Form from './components/Form';
 
 const projectList = document.getElementById('project-list');
 
 const form = document.querySelector('form');
 form.addEventListener('submit', e => {
-    Form.submitForm(form, e, projectList);
+    e.preventDefault();
+    const inputText = form.elements['project-input'].value;
+
+    const li = document.createElement('li');
+    li.classList.add('project');
+    li.textContent = inputText;
+    projectList.appendChild(li);
+    form.reset();
 })
 
 const formContainer = document.querySelector('.form-container');
 
-const addProjectBtn = document.getElementById('open-project-form');
+const addProjectBtn = document.getElementById('add-project-btn');
 addProjectBtn.addEventListener('click', () => {
-    Form.showForm(formContainer, addProjectBtn);
+    formContainer.hidden = false;
+    addProjectBtn.hidden = true;
 })
 
 const cancelProjectBtn = document.getElementById('cancel-create-project-btn');
 cancelProjectBtn.addEventListener('click', () => {
-    Form.hideForm(formContainer, addProjectBtn);
-    Form.resetForm(form);
+    formContainer.hidden = true;
+    addProjectBtn.hidden = false;
+    form.reset();
 })
 
 const createProjectBtn = document.getElementById('create-project-btn');
 createProjectBtn.addEventListener('click', () => {
-    Form.hideForm(formContainer, addProjectBtn);
+    formContainer.hidden = true;
+    addProjectBtn.hidden = false;
 })
