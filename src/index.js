93d9207577e3b7228c4283d080c6cf7fd91b87b2
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-19 20:11:25 -0500

    Create DOM for todo form and add listeners

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import { showElement, hideElement } from './helpers/visibilityChanger';
 import { submitForm, resetForm } from './helpers/formHandler';
 import Project from './classes/Project';
 
-const formContainer = document.querySelector('.form-container');
 
 // Project
+const projectFormContainer = document.getElementById('project-form-container');
+
 const projectForm = document.getElementById('create-project-form');
 projectForm.addEventListener('submit', event => {
     submitForm(event);
@@ -15,19 +16,47 @@ projectForm.addEventListener('submit', event => {
 
 const openProjectForm = document.getElementById('open-project-form');
 openProjectForm.addEventListener('click', () => {
-    showElement(formContainer);
+    showElement(projectFormContainer);
     hideElement(openProjectForm);
-})
-
-const cancelProjectBtn = document.getElementById('cancel-project-btn');
-cancelProjectBtn.addEventListener('click', () => {
-    hideElement(formContainer);
-    showElement(openProjectForm);
-    resetForm(projectForm);
 })
 
 const createProjectBtn = document.getElementById('create-project-btn');
 createProjectBtn.addEventListener('click', () => {
-    hideElement(formContainer);
+    hideElement(projectFormContainer);
     showElement(openProjectForm);
+})
+
+const cancelProjectBtn = document.getElementById('cancel-project-btn');
+cancelProjectBtn.addEventListener('click', () => {
+    hideElement(projectFormContainer);
+    showElement(openProjectForm);
+    resetForm(projectForm);
+})
+
+// Todo
+const todoFormContainer = document.getElementById('todo-form-container');
+
+const todoForm = document.getElementById('create-todo-form');
+todoForm.addEventListener('submit', event => {
+    submitForm(event);
+    console.log('todo submitted');
+})
+
+const openTodoForm = document.getElementById('open-todo-form');
+openTodoForm.addEventListener('click', () => {
+    showElement(todoFormContainer);
+    hideElement(openTodoForm);
+})
+
+const createTodoBtn = document.getElementById('create-todo-btn');
+createTodoBtn.addEventListener('click', () => {
+    hideElement(todoFormContainer);
+    showElement(openTodoForm);
+})
+
+const cancelTodoBtn = document.getElementById('cancel-todo-btn');
+cancelTodoBtn.addEventListener('click', () => {
+    hideElement(todoFormContainer);
+    showElement(openTodoForm);
+    resetForm(projectForm);
 })
